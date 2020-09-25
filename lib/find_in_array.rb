b05aa6c96fb630f62e46deb6f005684cae662a8a
Jordan Panasewicz <jbpanasewicz@gmail.com>
2020-09-25 19:30:22 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,9 @@
+# def find_element_index(array, value_to_find)
+#  array.index(value_to_find)
+# end
+
+#why would you want to loop that?? but okay....
+
 def find_element_index(array, value_to_find)
-  array.index(value_to_find)
-end
+  
+  
